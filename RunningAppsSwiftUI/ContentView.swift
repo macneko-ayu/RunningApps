74f7549b17d317a81177a56ded05af63ef8440f6
macneko-ayu <macneko.ayu@gmail.com>
2020-12-06 13:33:50 +0900

    Add target for SwiftUI

---
+++ RunningAppsSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  RunningAppsSwiftUI
+//
+//  Created by Kojiro Yokota on 2020/12/06.
+//  Copyright © 2020 横田孝次郎. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
