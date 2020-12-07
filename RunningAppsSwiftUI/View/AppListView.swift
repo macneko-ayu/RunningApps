a98754f882e4155c111531597ff423e200ec46b8
macneko-ayu <macneko.ayu@gmail.com>
2020-12-07 21:08:56 +0900

    Add view for application list

---
+++ RunningAppsSwiftUI/View/AppListView.swift
@@ -0,0 +1,21 @@
+//
+//  AppListView.swift
+//  RunningApps
+//
+//  Created by Kojiro Yokota on 2020/12/07.
+//  Copyright © 2020 macneko.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct AppListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppListView()
+    }
+}
