edeaa4dbe78030094eb74f30104a440689e445fc
macneko-ayu <macneko.ayu@gmail.com>
2020-12-08 23:19:35 +0900

    起動しているアプリケーションをViewに表示できるようにした

--- RunningAppsSwiftUI/View/ContentView.swift
+++ RunningAppsSwiftUI/View/ContentView.swift
@@ -3,15 +3,28 @@
 //  RunningAppsSwiftUI
 //
 //  Created by Kojiro Yokota on 2020/12/06.
-//  Copyright © 2020 横田孝次郎. All rights reserved.
+//  Copyright © 2020 macneko.com. All rights reserved.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject private var viewModel = RunningAppsViewModel()
+
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        ScrollView(.vertical, showsIndicators: false) {
+            ForEach(viewModel.metaData) { data in
+                Button(action: {
+                    guard let app = NSWorkspace.shared.runningApplications
+                        .filter ({ (app: NSRunningApplication) in app.activationPolicy == NSApplication.ActivationPolicy.regular })
+                        .filter ({ (app: NSRunningApplication) in app.bundleIdentifier == data.identifier && app.bundleURL?.path == data.url.path }).first
+                        else { return }
+                    app.activate(options: [])
+                }) {
+                    AppListView(metaData: data)
+                }
+            }
+        }
     }
 }
 
